c59ae41d93482d973297b956671ef6938382f164
Simas Jarmalavičius 20193574 <simas.jarmalavicius@stud.vilniustech.lt>
2024-11-13 21:42:28 +0200

    Adding Github authentication for users

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    $user = User::firstOrCreate([
+        'github_id' => $githubUser->getId(),
+    ], [
+        'name' => $githubUser->getName(),
+        'email' => $githubUser->getEmail(),
+        'avatar' => $githubUser->getAvatar(),
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/'); // Redirect to the home page after login
 });
